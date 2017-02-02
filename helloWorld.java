179220ebfd89db17f3424ac38d54cfce82902513
MikeyKulpa <kulpa7@gmail.com>
2017-02-01 22:49:08 -0600

    Learning java

---
+++ helloWorld.java
@@ -0,0 +1,23 @@
+package firstProject;
+
+/**
+ * Created by Mike on 1/31/2017.
+ */
+//class = group code together. Defines an object
+public class helloWorld{
+    public static void main( String[] args){
+        int mikey = 16;
+        double chen = -0.005;
+        char phone = 'J';
+        boolean dog = true;
+        final long cat = 94004022;
+        String Califorina = "Hi im mike";
+
+
+
+        System.out.println("I am " + mikey);
+
+    }
+
+
+}
